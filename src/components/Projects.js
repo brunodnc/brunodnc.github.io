c6fa66bbe54fb5f8e5ab7c64b8f2c50b04cc54b5
brunodnc <brunocecchini@hotmail.com>
2022-09-04 23:50:41 -0300

    refactor: updating components and json

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,34 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+const projectsJSON = require('../data/projects.json'); // tem que usar o FileReader...
 
 export const Projects = () => {
+  const [projects, setProjects] = useState(false);
+  const [stacks, setStacks] = useState([]);
+  const [filters, setFilters] = useState([]);
+  const [filteredProjects, setFilteredProjects] = useState([]);
+  
+  useEffect(() => { //set projects on load
+    setProjects(projectsJSON.projects);
+    setFilteredProjects(projectsJSON.projects);
+  }, []);
+
+  useEffect(() => { // get stack list
+    if (projects) {
+      const _stacks = projects.reduce((prev, cur) => {
+        for (let stack of cur.stacks) {
+          if (!prev.contains(stack)) {
+             prev.push(stack);
+          }
+        }
+        return prev;
+      }, []);
+      setStacks(_stacks);
+    }
+  },[projects])
+
+  useEffect(() => {
+
+  }, filters)
+
     return (
     <article id="projects">
-    <h2>Projects</h2>
-    <section class="project-tile" id="project-tile">
-      <img src="https://i.postimg.cc/d3nbzjNc/fcc1.png" /><a
-        href="https://codepen.io/brunodnc/pen/dyVMmLy"
-        target="_blank" rel="noreferrer"><h3>Tribute Page</h3></a>
-    </section>
-    <section class="project-tile" id="tile2">
-      <img src="https://i.postimg.cc/y6PCg71p/fcc2.png" />
-      <a href="https://codepen.io/brunodnc/pen/mdBwxeP"
-      target="_blank" 
-      rel="noreferrer">
-          <h3>Survey</h3>
-      </a>
-    </section>
-    <section class="project-tile" id="tile3">
-      <img src="https://i.postimg.cc/zvJmtcjk/fcc3.png" />
-      <a href="https://codepen.io/brunodnc/pen/KKXoELp"
-        target="_blank" rel="noreferrer"><h3>Landing Page</h3></a>
-    </section>
-    <section class="project-tile" id="tile4">
-      <img src="https://i.postimg.cc/bJ0cRhQJ/fcc4.png" /><a
-        href="https://codepen.io/brunodnc/pen/mdBLozx"
-        target="_blank" rel="noreferrer"
-        ><h3>Technical Documentation Page</h3></a
-      >
-    </section>
-  </article>
+      <h2>Projects</h2>
+      <div class="filter-bar">
+        {stacks.map((s) => (
+          <button 
+            key={s}
+            type="button"
+            class="filter-btn"
+            onClick={(e) => {
+              setFilters([...filters, s])
+            }}
+          >
+            {s}
+          </button>
+        ))}
+      </div>
+      {filteredProjects.map((p) => (
+        <section class="project-tile" key={p.id}>
+          <h3>{p.name}</h3>
+          <p>{p.description}</p>
+          <a href={p.link}>{ p.link.includes('github') ? 'Live Version' : 'Code' }</a>
+          <ul>{p.stacks.map((s) => <li key={s}>{s}</li>)}</ul>
+        </section>
+      ))}
+      <h2>Exercises</h2>
+    </article>
   )
 }
